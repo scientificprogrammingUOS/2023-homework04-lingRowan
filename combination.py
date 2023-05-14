e3356ce51b5e8f2826115adbf23c644618fc4ab1
lingRowan <rowanahmed880@gmail.com>
2023-05-14 20:26:21 +0200

    initial command

--- combination.py
+++ combination.py
@@ -2,12 +2,17 @@ import numpy as np
 
 # implement your function to combine two numpy arrays 
 
-def combination(a,b):
-    combining = np.concatenate((a,b))
-    return combining
+def combination(arr1, arr2, method='product'):
+    if method == 'product':
+        return arr1 * arr2.reshape(1, 1, 2, 2)
+    elif method == 'sum':
+        return arr1 + arr2.reshape(1, 1, 2, 2)
+    else:
+        raise ValueError('Invalid combination method specified.')
 
 
 if __name__ == "__main__":
     # use this for your own testing!
-    print(combination([1,6,9],[3,5,9]))
+   #print(combination([1,6,9],[3,5,9]))
+   pass
     
--- strange_pattern.py
+++ strange_pattern.py
@@ -3,12 +3,7 @@ import numpy as np
 # implement the function strange pattern
 
 def strange_pattern(size):
-<<<<<<< HEAD
-=======
-    # delete the NotImplementedError when you write your function.
-    #arr = np.empty(shape = (m,n))
-    #bool_array = np.array(arr, dtype = 'bool')
->>>>>>> e5c3f973a73d89a49e128c0f7c925c93bccd02ca
+    
     bool_array = np.random.choice([True,False], size = size)
     return bool_array
 
