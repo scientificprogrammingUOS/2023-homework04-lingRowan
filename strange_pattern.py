e5c3f973a73d89a49e128c0f7c925c93bccd02ca
lingRowan <rowanahmed880@gmail.com>
2023-05-14 19:57:22 +0200

    initial command

--- strange_pattern.py
+++ strange_pattern.py
@@ -2,14 +2,15 @@ import numpy as np
 
 # implement the function strange pattern
 
-def strange_pattern(m , n):
+def strange_pattern(size):
     # delete the NotImplementedError when you write your function.
     #arr = np.empty(shape = (m,n))
     #bool_array = np.array(arr, dtype = 'bool')
-    bool_array = np.random.choice([True,False], size = (m,n))
+    bool_array = np.random.choice([True,False], size = size)
     return bool_array
 
 
 if __name__ == "__main__":
-    print(strange_pattern(2,3))
+    
+    print(strange_pattern((2,3)))
     pass
