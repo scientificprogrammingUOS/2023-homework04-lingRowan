b828062542a6522c0aecf5b3d8a40bd9c3d7b61d
lingRowan <rowanahmed880@gmail.com>
2023-05-14 20:28:08 +0200

    initial command

--- combination.py
+++ combination.py
@@ -2,17 +2,12 @@ import numpy as np
 
 # implement your function to combine two numpy arrays 
 
-def combination(arr1, arr2, method='product'):
-    if method == 'product':
-        return arr1 * arr2.reshape(1, 1, 2, 2)
-    elif method == 'sum':
-        return arr1 + arr2.reshape(1, 1, 2, 2)
-    else:
-        raise ValueError('Invalid combination method specified.')
+def combination(a,b):
+    combining = np.concatenate((a,b))
+    return combining
 
 
 if __name__ == "__main__":
     # use this for your own testing!
-   #print(combination([1,6,9],[3,5,9]))
-   pass
+    print(combination([1,6,9],[3,5,9]))
     
